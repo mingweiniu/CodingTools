93d4b1a196d65b4b5bfbb4992693b76d99bb2e05
Sidney Niu <mingweiniu@gmail.com>
2019-10-15 13:54:02 +0800

    Update opencv.bat

--- others/opencv.bat
+++ others/opencv.bat
@@ -5,8 +5,9 @@ cd ..
 git clone https://github.com/Itseez/opencv.git
 cd opencv
 git checkout ${CV_VERSION}
-mkdir build && cd build && \
-          -D CMAKE_INSTALL_PREFIX=../install \
+mkdir build 
+cd build 
+cmake -D CMAKE_INSTALL_PREFIX=../install \
           -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
           -D INSTALL_C_EXAMPLES=OFF \
           -D INSTALL_PYTHON_EXAMPLES=OFF \
